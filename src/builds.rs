8602b15e128c2c16063f5d93f42cf43945d4ad39
Adrian Taylor <adetaylor@chromium.org>
2022-11-24 15:07:25 +0000

    Lots of code.

---
+++ src/builds.rs
@@ -0,0 +1,55 @@
+use anyhow::Result;
+use serde::Deserialize;
+
+const BUCKET: &str = "https://commondatastorage.googleapis.com/chromium-browser-asan/";
+
+#[derive(Deserialize, Debug)]
+struct Content {
+    #[serde(rename = "Key")]
+    key: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ListBucketResult {
+    #[serde(rename = "Contents")]
+    contents: Vec<Content>,
+}
+
+pub(crate) struct BuildSpecification<'a> {
+    pub(crate) build_type: &'a str,
+    pub(crate) platform: &'a str,
+    pub(crate) debugness: &'a str,
+}
+
+pub(crate) fn format_prefix(specification: &BuildSpecification, version: &str) -> String {
+    let build_type = specification.build_type;
+    let platform = specification.platform;
+    let debugness = specification.debugness;
+    format!("{platform}-{debugness}/{build_type}-{platform}-{debugness}-{version}")
+}
+
+pub(crate) fn get_builds(
+    specification: &BuildSpecification,
+    version_prefix: &str,
+) -> Result<Vec<u64>> {
+    let prefix = format_prefix(specification, version_prefix);
+    let uri = format!("{BUCKET}?prefix={prefix}");
+    let response = reqwest::blocking::get(uri)?;
+    let bucket_result: ListBucketResult = serde_xml_rs::from_reader(response)?;
+    let prefix_to_remove = format_prefix(specification, "").len();
+    Ok(bucket_result
+        .contents
+        .into_iter()
+        .filter_map(|content| {
+            let build_label = content.key;
+            let version_slice = &build_label[prefix_to_remove..build_label.len() - 4];
+            version_slice.parse::<u64>().ok()
+        })
+        .collect())
+}
+
+pub(crate) fn get_download_uri(specification: &BuildSpecification, version: u64) -> String {
+    let prefix = format_prefix(specification, &format!("{}", version));
+    let prefix = url_escape::encode_component(&prefix);
+    format!("https://www.googleapis.com/download/storage/v1/b/chromium-browser-asan/o/{prefix}.zip&alt=media")
+}
---
+++ src/releases.rs
@@ -0,0 +1,44 @@
+
+use anyhow::Result;
+use indexmap::IndexMap;
+use serde::Deserialize;
+
+const URI: &str = "https://chromiumdash.appspot.com/fetch_releases";
+
+#[derive(Deserialize, Debug)]
+struct Release {
+    chromium_main_branch_position: u64,
+    milestone: u16,
+    channel: String,
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct ChannelInfo {
+    pub(crate) milestone: u16,
+    pub(crate) chromium_main_branch_position: u64,
+}
+
+pub(crate) fn get_channel_branch_positions() -> Result<IndexMap<String, ChannelInfo>> {
+    let response = reqwest::blocking::get(URI)?;
+    let releases: Vec<Release> = serde_json::from_reader(response)?;
+    let mut channels: IndexMap<String, ChannelInfo> = IndexMap::new();
+    for release in releases.into_iter() {
+        let chromium_main_branch_position = release.chromium_main_branch_position;
+        let milestone = release.milestone;
+        channels
+            .entry(release.channel)
+            .and_modify(|existing| {
+                if milestone > existing.milestone {
+                    *existing = ChannelInfo {
+                        milestone,
+                        chromium_main_branch_position,
+                    }
+                }
+            })
+            .or_insert(ChannelInfo {
+                milestone,
+                chromium_main_branch_position,
+            });
+    }
+    Ok(channels)
+}
