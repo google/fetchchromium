610b994e841cd4c5b12bab8ed30a9c3c6209e939
Adrian Taylor <adetaylor@chromium.org>
2024-05-15 18:00:23 +0100

    Fix unrar spelling

--- setup-blank-vm.sh
+++ setup-blank-vm.sh
@@ -17,7 +17,7 @@ sudo dpkg -i chrome-remote-desktop_125.0.6422.20_amd64.deb
 rm chrome-remote-desktop_125.0.6422.20_amd64.deb
 
 sudo DEBIAN_FRONTEND=noninteractive apt --fix-broken --assume-yes install
-sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes xfce4 llvm desktop-base dbus-x11 xscreensaver unzip unrar
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes xfce4 llvm desktop-base dbus-x11 xscreensaver unzip unrar-free
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 
 echo "Restarting the VM - please SSH in again, then run fetchchromium with whatever options you like (possibly none)"
