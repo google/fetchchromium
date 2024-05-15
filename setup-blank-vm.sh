e1029c5dc6adca1909e01778f61de9406d3bd26c
Adrian Taylor <adetaylor@chromium.org>
2024-05-15 14:37:34 +0100

    Script cleanups

--- setup-blank-vm.sh
+++ setup-blank-vm.sh
@@ -5,10 +5,12 @@ wget "https://raw.githubusercontent.com/chromium/chromium/main/tools/valgrind/as
 
 wget "https://github.com/google/fetchchromium/releases/download/v0.4.0/fetchchromium_0.4.0-1_amd64.deb"
 sudo dpkg -i fetchchromium_0.4.0-1_amd64.deb
+rm fetchchromium_0.4.0-1_amd64.deb
 
 # Hard coded because of a bug in some versions
 wget "https://deb.rug.nl/ppa/mirror/dl.google.com/linux/chrome-remote-desktop/deb/pool/main/c/chrome-remote-desktop/chrome-remote-desktop_125.0.6422.20_amd64.deb"
 sudo dpkg -i chrome-remote-desktop_125.0.6422.20_amd64.deb
+rm chrome-remote-desktop_125.0.6422.20_amd64.deb
 
 sudo DEBIAN_FRONTEND=noninteractive apt --fix-broken --assume-yes install
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes xfce4 llvm desktop-base dbus-x11 xscreensaver unzip unrar
