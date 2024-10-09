f3354f47001cb4a33a25b592c6b18395aff99d1b
Adrian Taylor <adetaylor@chromium.org>
2024-10-09 10:15:44 +0100

    Install binutils.
    This is needed for asan_symbolize to work because it needs addr2line.
    
    Also, upgrading ripunzip to support passwords.

--- setup-blank-vm.sh
+++ setup-blank-vm.sh
@@ -7,9 +7,9 @@ wget "https://github.com/google/fetchchromium/releases/download/v0.4.1/fetchchro
 sudo dpkg -i fetchchromium_0.4.1-1_amd64.deb
 rm fetchchromium_0.4.1-1_amd64.deb
 
-wget "https://github.com/google/ripunzip/releases/download/v1.2.2/ripunzip_1.2.2-1_amd64.deb"
-sudo dpkg -i ripunzip_1.2.2-1_amd64.deb
-rm ripunzip_1.2.2-1_amd64.deb
+wget "https://github.com/google/ripunzip/releases/download/v2.0.0/ripunzip_2.0.0-1_amd64.deb"
+sudo dpkg -i ripunzip_2.0.0-1_amd64.deb
+rm ripunzip_2.0.0-1_amd64.deb
 
 # Hard coded because of a bug in some versions
 wget "https://deb.rug.nl/ppa/mirror/dl.google.com/linux/chrome-remote-desktop/deb/pool/main/c/chrome-remote-desktop/chrome-remote-desktop_125.0.6422.20_amd64.deb"
@@ -18,7 +18,7 @@ rm chrome-remote-desktop_125.0.6422.20_amd64.deb
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt --fix-broken --assume-yes install
-sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes xfce4 llvm desktop-base dbus-x11 xscreensaver unzip unrar-free
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes xfce4 llvm desktop-base dbus-x11 xscreensaver unzip unrar-free binutils
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 
 echo "Enabling unprivileged user namespaces so Chrome can sandbox itself. Note that this slightly weakens the security of this VM's kernel against local attackers."
